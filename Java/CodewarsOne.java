8e15f048e9441030879ea2c2a5efe476eb13e2a9
Miguel Vera <miguel.vera7908@gmail.com>
2020-04-30 21:55:18 -0500

    update

--- Java/CodewarsOne.java
+++ Java/CodewarsOne.java
@@ -121,4 +121,15 @@ public class CodewarsOne {
             return false;
         }
     }
+
+    // TODO: Problem 2
+// Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+    public static boolean icyHot(int temp1, int temp2) {
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
